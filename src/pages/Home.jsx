82f5c78258823dcff18b02462d7ff01bc4d13582
Jhojan Galvis <sayrakatalinagalvis@gmail.com>
2025-06-16 20:53:30 -0500

    configuracion prime react y creaction copmponennte home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Button } from "primereact/button"
+import { Calendar } from "primereact/calendar"
+
+function Home() {
+  return (
+    <div>
+        Primcipal
+        <Button label="Primary" />
+        <Calendar  />
+    </div>
+  )
+}
+
+export default Home
