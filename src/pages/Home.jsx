1ecec5e91683c21e2b9cf5d5cc62110dc7244a09
Jhojan Galvis <sayrakatalinagalvis@gmail.com>
2025-06-17 21:04:23 -0500

    Se realizo la vista del header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,6 @@ import { Calendar } from "primereact/calendar"
 function Home() {
   return (
     <div>
-        Primcipal
-        <Button label="Primary" />
-        <Calendar  />
     </div>
   )
 }
--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -1,15 +1,30 @@
 import '../styles/Servicios.css';
 
-function Header () {
+function Header (imagen) {
     return (
-        <div>
-            <button>Inicio</button>
-            <button>Nosotros</button>
-            <button>Productos</button>
-            <button>Servicios</button>
-            <button>Contactenos</button>
-            <button>Iniciar Sesion</button>
-        </div>
+<header className="header">
+  <div className="header-top">
+    <div className="header-logo">
+      <img src="Logo.png" className="Imagen" alt="Logo"/>
+    </div>
+    <div className="search-bar">
+      <input type="text" placeholder="Buscar productos o servicios..." />
+      <button>
+        🔍
+      </button>
+    </div>
+  </div>
+  <nav className="navbar">
+    <div className="nav-links">
+      <a href="#">INICIO</a>
+      <a href="#">NOSOTROS</a>
+      <a href="#">PRODUCTOS</a>
+      <a href="#" className="active">SERVICIOS</a>
+      <a href="#">CONTACTENOS</a>
+    </div>
+    <a href="#" className="login-button">Iniciar Sesión</a>
+  </nav>
+</header>
     )
 }
 
