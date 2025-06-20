71c557a10831e7d5b29b30270e202c12a2f6b110
Cristian-0923 <camilocastelcubi@gmail.com>
2025-06-19 20:58:06 -0500

    creacion de rama cambiar contraseña

---
+++ src/pages/CambiarContraseña/CambiarContraseña.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+import img from './img.png'
+import './IniciarSesion.css'
+
+function IniciarSesion(){
+    const [correo, setCorreo] = useState('');
+    const [contraseña, setContraseña] = useState('');
+    
+    return(
+        <div className="container-principal">
+            <div className="caja-sesion">
+                <div className="section-logo">
+                    <img src={img} alt="" />
+                </div>
+                <div className="section-informacion">
+                    <h2>Iniciar sesion</h2>
+                    <p>Ingresar tus credenciales para ingresar a tu cuenta</p>
+                    <FloatLabel>
+                        <InputText id="correo" value={correo} onChange={(e) => setCorreo(e.target.value)} />
+                        <label htmlFor="correo">Correo:</label>
+                    </FloatLabel>
+                    <FloatLabel>
+                        <InputText id="contraseña" type='password' value={contraseña} onChange={(e) => setContraseña(e.target.value)} />
+                        <label htmlFor="contraseña">Contraseña:</label>
+                    </FloatLabel>
+                    <button type="button">Iniciar sesion </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default IniciarSesion;
