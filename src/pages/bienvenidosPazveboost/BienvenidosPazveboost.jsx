d39ef935c4ae3bcf3e0e02babe5cd78503016423
FabianC2007 <edison.caicedoo2007@gmail.com>
2025-06-24 18:56:49 -0500

    interfaz bienvenidos a pazveboost terminada

--- src/pages/bienvenidosPazveboost/BienvenidosPazveboost.jsx
+++ src/pages/bienvenidosPazveboost/BienvenidosPazveboost.jsx
@@ -7,7 +7,11 @@ import img from "./img.png"
 function BienvenidosPazveboost(){
     return(
         <div className="cdr_inf">
-            <img src={img}/>
+            <p><strong><h1>¡¡Bienvenidos A <br/>PazveBoost!!</h1><br/><img src={img}/><br/>
+            Tu tranquiliad es nuestra prioridad. Encuentra los <br/>mejores servicios de instalacion y monitoreo de<br/> camaras de seguridad para proteger lo que mas te<br/> importa.
+            </strong><br/><br/>
+            <button>Siguiente</button>
+            </p>
           
         </div>
     )
