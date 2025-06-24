a15ba4de98207949ae1a17c023ab73ad74b47647
smoreno <joamorenoplay8@gmail.com>
2025-06-24 18:56:25 -0500

    Contenedores-Carrito

--- src/pages/carritodecompras/ShoppingCart.jsx
+++ src/pages/carritodecompras/ShoppingCart.jsx
@@ -6,28 +6,31 @@ const ShoppingCart = () => {
     const totalCost = "$329.900 COP";
 
     return (
-        <div className="shopping-cart-container">
-            <p className="title">Información de la Compra</p>
+        <div className='container'>
+            <div className='header-shopping-cart-container'></div>
+            <div className="shopping-cart-container">
+                <p className="title">Información de la Compra</p>
 
-            <div className="item">
-                <span className="item-label">Productos</span>
-                <span className="arrow">➔</span>
-                <span className="item-value">{productPrice}</span>
-                <button className="action-button primary">Comprar ahora</button>
-            </div>
+                <div className="item">
+                    <span className="item-label">Productos</span>
+                    <span className="arrow">➔</span>
+                    <span className="item-value">{productPrice}</span>
+                    <button className="action-button primary">Comprar ahora</button>
+                </div>
 
-            <div className="item">
-                <span className="item-label">Envio</span>
-                <span className="arrow">➔</span>
-                <span className="item-value green">{shippingCost}</span>
-                <div className="button-placeholder"></div>
-            </div>
+                <div className="item">
+                    <span className="item-label">Envio</span>
+                    <span className="arrow">➔</span>
+                    <span className="item-value green">{shippingCost}</span>
+                    <div className="button-placeholder"></div>
+                </div>
 
-            <div className="item">
-                <span className="item-label">Total</span>
-                <span className="arrow">➔</span>
-                <span className="item-value">{totalCost}</span>
-                <button className="action-button secondary">Descarga tu factura</button>
+                <div className="item">
+                    <span className="item-label">Total</span>
+                    <span className="arrow">➔</span>
+                    <span className="item-value">{totalCost}</span>
+                    <button className="action-button secondary">Descarga tu factura</button>
+                </div>
             </div>
         </div>
     )
