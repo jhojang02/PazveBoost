d9080eafa9f1399f0e5428208daea54725f8974f
FabianM21 <fabianmantilla1622@gmail.com>
2025-06-26 21:04:04 -0500

    se da color al Editar perfil

--- src/pages/EditarPerfil.jsx
+++ src/pages/EditarPerfil.jsx
@@ -1,37 +1,44 @@
 import React from 'react'; 
 import { Avatar } from 'primereact/avatar';
 import { Badge } from 'primereact/badge';
-import "../pages/Perfil.css";
 import 'primeicons/primeicons.css';
 import { InputText } from 'primereact/inputtext';
 import { FloatLabel } from 'primereact/floatlabel';
+import '../pages/EditarPerfil.css';
 
 function EditarPerfil() {
 const [value, setValue] = React.useState('');
-    return (
+    
+return (
         <div className='Contenedor'>
+            <i className='pi pi-arrow-left back-arrow'/>
             <div className="card">
             <div className="flex flex-wrap gap-5">
                 <div className="flex-auto">
                     <h2 className='Titulo'> Editar Perfil</h2>
-                    <Avatar image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png" className="mr-2" size="xlarge" shape="circle" />
-                    
+                    <Avatar className="x-avatar" image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png"  size="xlarge" shape="circle" />
+                    <div className='labels-u'>
+                        <div className='espaciado'>
                     <FloatLabel>
-                    <InputText id="Nombre U" value={value} onChange={(e) => setValue(e.target.value)} />
-                    <label htmlFor="username">Username</label>
+                    <InputText id="nombre_u" value={value} onChange={(e) => setValue(e.target.value)} />
+                    <label htmlFor="username">Nombre Usuario</label>
                     </FloatLabel>
+                        </div>
 
+                        <div className='espaciado'>
                     <FloatLabel>
                     <InputText id="username" value={value} onChange={(e) => setValue(e.target.value)} />
                     <label htmlFor="username">Correo</label>
                     </FloatLabel>
-
+                        </div>
+                        <div className='espaciado'>
                     <FloatLabel>
                     <InputText id="username" value={value} onChange={(e) => setValue(e.target.value)} />
                     <label htmlFor="username">Celular</label>
                     </FloatLabel>
+                    </div>
+                    </div>
 
-                    
                 </div>
         </div>
         </div>
