3bd9f0e62e0822fdb9a049bbd554566ca55c9369
Jhojan Galvis <sayrakatalinagalvis@gmail.com>
2025-07-03 20:01:58 -0500

    Cambios en el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import EditarPerfil from './pages/EditarPerfil.jsx';
-import EditarPerfil from './pages//EditarPerfil.jsx';
-import Perfil from './pages//Perfil.jsx';
+import Perfil from './pages/Perfil.jsx';
 
 
 function App() {
