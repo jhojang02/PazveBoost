b7ccee05c9ecd33baf0a41261e68a6d0a6f052f2
FabianM21 <fabianmantilla1622@gmail.com>
2025-07-04 20:10:58 -0500

    cambios en la pagina iniciar sesion

--- src/pages/Iniciar-sesion/IniciarSesion.jsx
+++ src/pages/Iniciar-sesion/IniciarSesion.jsx
@@ -5,32 +5,45 @@ import { FloatLabel } from "primereact/floatlabel";
 import img from './img.png'
 import './IniciarSesion.css'
 
-function IniciarSesion(){
+function IniciarSesion({ esEmpleado }) {
     const [correo, setCorreo] = useState('');
     const [contraseña, setContraseña] = useState('');
-    
-    return(
+
+
+
+
+    return (
         <div className="container-principal">
             <div className="caja-sesion">
                 <div className="section-logo">
                     <img src={img} alt="" />
                 </div>
                 <div className="section-informacion">
-                    <h2>Iniciar sesion</h2>
+                    <h2>Iniciar sesion {esEmpleado ? 'empleado' : 'cliente'}</h2>
                     <p>Ingresar tus credenciales para ingresar a tu cuenta</p>
                     <FloatLabel>
                         <InputText id="correo" value={correo} onChange={(e) => setCorreo(e.target.value)} />
-                        <label htmlFor="correo">Correo:</label>
+                        <label htmlFor="correo">{esEmpleado ? 'Nombre de usuario' : 'Correo:'}</label>
                     </FloatLabel>
                     <FloatLabel>
                         <InputText id="contraseña" type='password' value={contraseña} onChange={(e) => setContraseña(e.target.value)} />
                         <label htmlFor="contraseña">Contraseña:</label>
                     </FloatLabel>
-                    <button type="button">Iniciar sesion </button>
+                    <button type="button" onClick={() => enviarDatos(esEmpleado)}>
+                        Iniciar sesión
+                    </button>
                 </div>
             </div>
         </div>
     )
+}
+
+function enviarDatos(esEmpleado) {
+    if (esEmpleado) {
+        alert("Es empleado")
+    } else {
+        alert("esCliente")
+    }
 }
 
 export default IniciarSesion;
