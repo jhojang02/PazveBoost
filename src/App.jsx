9180f491f5884ab1156dcb60cf12c20b3c9bde59
FabianM21 <fabianmantilla1622@gmail.com>
2025-07-07 19:41:44 -0500

    Otros cambios en el App

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     
-    <PrimeReactProvider value={{ unstyled: false }}>
+    <PrimeReactProvider value={{ unstyled: true }}>
       <Home />
     </PrimeReactProvider>
   )
