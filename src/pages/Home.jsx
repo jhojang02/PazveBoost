90281b74a7c533ab5f220683c01b95a0d5e14aed
FabianC2007 <edison.caicedoo2007@gmail.com>
2025-07-28 20:02:25 -0500

    actualizacion ramas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import { Button } from "primereact/button"
 import { Calendar } from "primereact/calendar"
 import InformacionUsuario from "./inf_usuario/InformacionUsuario.jsx"
-import OlvidasteContraseña from "./olvidasteContraseña/OlvidasteContraseña.jsx"
-import Bienvenidos from "./bienvenidosPazveboost/BienvenidosPazveboost.jsx"
+
 
 
 function Home() {
