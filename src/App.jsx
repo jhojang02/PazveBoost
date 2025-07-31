8c588f220a95d58703e1ee125ccd7d4f7e5fe234
Galvis <sayrakatalinagalvis@gmail.com>
2025-07-31 09:23:10 -0500

    Se arregla la pantalla de bienvenida

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
      
 
     <PrimeReactProvider value={{ unstyled: false }}>
-      <InfoUser />
+      <Home />
     </PrimeReactProvider>
     
    
--- src/pages/bienvenidosPazveboost/BienvenidosPazveboost.jsx
+++ src/pages/bienvenidosPazveboost/BienvenidosPazveboost.jsx
@@ -6,8 +6,8 @@ import img from "./img.png"
 
 function BienvenidosPazveboost(){
     return(
-        <div className="cdr_inf">
-            <p><strong><h1>¡¡Bienvenidos A <br/>PazveBoost!!</h1><img src={img}/><br/>
+        <div className="ContenedorBienvenida">
+            <p><strong><h1>¡¡Bienvenidos A <br/>PazveBoost!!</h1><img src={img} className="ImagenBienvenida"/><br/>
             Tu tranquiliad es nuestra prioridad. Encuentra los <br/>mejores servicios de instalacion y monitoreo de<br/> camaras de seguridad para proteger lo que mas te<br/> importa.
             </strong><br/><br/>
             <button>Siguiente</button>
