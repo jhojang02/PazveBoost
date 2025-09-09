f50abb0239ef876b3c41166834ad3969a5eaf336
Jhojan Galvis <sayrakatalinagalvis@gmail.com>
2025-09-08 20:48:32 -0500

    Primera navegavilidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import './App.css'
-import "./index.css"
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import './App.css';
+import './index.css';
+import { PrimeReactProvider } from 'primereact/api';
+import { Routes, Route } from 'react-router-dom';
+
 import HomeBienvenida from './pages/Home';
-import InfoUser from './pages/inf_usuario/InformacionUsuario'
-import OlvidoContra from './pages/olvidasteContraseña/OlvidasteContraseña'
+import InfoUser from './pages/inf_usuario/InformacionUsuario';
+import OlvidoContra from './pages/olvidasteContraseña/OlvidasteContraseña';
 import Pantalla_ayuda from './pages/Pantalla_ayuda/Ayuda';
 
 function App() {
-
   return (
-     
-
     <PrimeReactProvider value={{ unstyled: false }}>
-      <Pantalla_ayuda />
+      <Routes>
+        <Route path="/" element={<HomeBienvenida />} />
+        <Route path="/usuario" element={<InfoUser />} />
+        <Route path="/olvidaste" element={<OlvidoContra />} />
+        <Route path="/ayuda" element={<Pantalla_ayuda />} />
+        <Route path="*" element={<div>Página no encontrada</div>} />
+      </Routes>
     </PrimeReactProvider>
-    
-   
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/bienvenidosPazveboost/BienvenidosPazveboost.jsx
+++ src/pages/bienvenidosPazveboost/BienvenidosPazveboost.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
-import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
 import "./BienvenidosPazveboost.css"
 import img from "./img.png"
+import { useNavigate } from "react-router-dom"
 
 function BienvenidosPazveboost(){
+    const navigate = useNavigate();
+
+    const handClick = () => {
+        navigate('/usuario');
+    };
+
     return(
         <div className="ContenedorBienvenida">
             <p><strong><h1>¡¡Bienvenidos A <br/>PazveBoost!!</h1><img src={img} className="ImagenBienvenida"/><br/>
             Tu tranquiliad es nuestra prioridad. Encuentra los <br/>mejores servicios de instalacion y monitoreo de<br/> camaras de seguridad para proteger lo que mas te<br/> importa.
             </strong><br/><br/>
-            <button>Siguiente</button>
+            <button onClick={handClick}>Siguiente</button>
             </p>
           
         </div>
