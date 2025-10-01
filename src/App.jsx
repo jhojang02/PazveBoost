c063ab9f0a7040148f54bb32ca1c8b25b9edd825
Galvis <sayrakatalinagalvis@gmail.com>
2025-10-01 10:18:15 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
-import Home from './pages/Home';
+
 
 function App() {
 
   return (
     
     <PrimeReactProvider value={{ unstyled: true }}>
-      <Home />
+
     </PrimeReactProvider>
   )
 }
