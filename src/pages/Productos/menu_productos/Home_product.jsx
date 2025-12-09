6dc36f9d109555f6500eefd031116585b698949d
Galvis <sayrakatalinagalvis@gmail.com>
2025-12-08 19:14:27 -0500

    Mas cambios visuales

--- src/pages/Productos/menu_productos/Home_product.jsx
+++ src/pages/Productos/menu_productos/Home_product.jsx
@@ -1,11 +1,11 @@
 import ProductCard from "../../../components/ProductosCard";
-import { products } from "../../../data/products";
+import { productspage } from "../../../data/productspage";
 import "./Home_product.css"
 
 function Home () {
   return (
     <div className="product-grid">
-        {products.map((product) => (
+        {productspage.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
     </div>
