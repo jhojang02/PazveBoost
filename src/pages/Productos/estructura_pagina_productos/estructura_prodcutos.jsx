781fcaf3c9777956eb8872843c91347737b536f6
FabianC2007 <edison.caicedoo2007@gmail.com>
2025-12-10 19:40:55 -0500

    modificacion y arreglos css

--- src/pages/Productos/estructura_pagina_productos/estructura_prodcutos.jsx
+++ src/pages/Productos/estructura_pagina_productos/estructura_prodcutos.jsx
@@ -17,20 +17,28 @@ function Estructura() {
         <Header busqueda={busqueda} setBusqueda={setBusqueda} />
 
         {/* Contenedor principal */}
-        <div className="body">
-          {/* Sidebar */}
-          <aside className="sidebar">
-            <CarritoCompras />
-            <BasicDemo selectedCity={selectedCity} setSelectedCity={setSelectedCity} />
-            <AccesosRapidos />
-            <WithoutCancelDemo />
-          </aside>
-
-          {/* Contenido principal */}
-          <main className="contenedor_fondo">
-            <Home />
-          </main>
+ 
+            <div className='body'>
+        <div className='sidebar'>
+          <div className='carrito-container'>
+          <CarritoCompras/>
+          </div>
+          <div className='carrito-header'>
+          <h3 className='name_car'>Carrito <span className='simbolo'>➡</span> </h3> 
+          <BasicDemo />
+          <AccesosRapidos />
+          <div className='estrellas'>
+            <h4>! Califica nuestra pagina ¡  😊  </h4>
+          <WithoutCancelDemo />
+          </div>
         </div>
+        </div>
+
+        <div className='contenedor contenedor_fondo'>
+            <Home />
+        </div>
+      </div>
+
       </CartProvider>
     </PrimeReactProvider>
   );
